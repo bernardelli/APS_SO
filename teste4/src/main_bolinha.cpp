63ac1ce50397974e6cbb35b4c314fdfec83b70f0
Rafael Bernardelli <rafael.bernardelli@gmail.com>
2017-06-18 22:30:34 -0300

    workinggit status

--- teste4/src/main_bolinha.cpp
+++ teste4/src/main_bolinha.cpp
@@ -7,6 +7,7 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>  // OpenCV window I/O
 #include <opencv2/shape.hpp>
+#include <unistd.h>
 
 
 using namespace std;
@@ -16,13 +17,24 @@ const char* filename = "../aps_repo/data/IMG_3691.MOV";
 
 
 float calc_position(Mat& frame);
+void marker_setup();
+float convert_to_height(cv::Point2f& point);
 
-Mat element = getStructuringElement( MORPH_ELLIPSE,
+Mat element_M = getStructuringElement( MORPH_ELLIPSE,
+                                     Size( 5, 5 ));
+Mat element_P = getStructuringElement( MORPH_ELLIPSE,
                                      Size( 3, 3 ));
+
+VideoCapture video_capture;//video_capture(filename);
+
+Mat frame;
+
+Point2f origin, y_vec;
+
 int main(int argc, char** argv )
 {
+    video_capture.open(1);
 
-    VideoCapture video_capture(0);//video_capture(filename);
 
 
     if (!video_capture.isOpened())
@@ -37,16 +49,18 @@ int main(int argc, char** argv )
 
 
 
-    namedWindow("video", WINDOW_GUI_NORMAL);
-    namedWindow("debug1", WINDOW_GUI_NORMAL);
-    namedWindow("debug2", WINDOW_GUI_NORMAL);
+    //namedWindow("video", WINDOW_GUI_NORMAL);
+    //namedWindow("debug1", WINDOW_GUI_NORMAL);
+    //namedWindow("debug2", WINDOW_GUI_NORMAL);
+
+
+
+
+
+    marker_setup();
+
+
     namedWindow("debug3", WINDOW_GUI_NORMAL);
-
-    Mat frame;
-
-
-
-
     for(;;) //Show the image captured in the window and repeat
     {
 
@@ -57,13 +71,14 @@ int main(int argc, char** argv )
             cout << " < < <  Game over!  > > > ";
             break;
         }
-        imshow("video", frame);
+        //
 
 
         float altura = calc_position(frame);
+        cout << "posicao: " << altura << endl;
         waitKey(1);
 
-        //cout << "posicao: " << altura << endl;
+
 
     }
 
@@ -71,27 +86,166 @@ int main(int argc, char** argv )
 }
 
 
+void marker_setup()
+{
+//find position of calibration blobs
+
+    namedWindow("calibrate", WINDOW_GUI_NORMAL);
+    Mat hsv_frame, violet_mask;
+    int c  = '0';
+    vector<vector<Point> > contours;
+    vector<Point2f> centers;
+    vector<float> radii;
+    while( 1)
+    {
+
+        video_capture >> frame;
+
+
+
+        boxFilter(frame, frame, frame.type(), Size(3,3));
+        cvtColor(frame, hsv_frame, CV_BGR2HSV);
+
+        Scalar hsv_lower_1(100, 100, 30);
+        Scalar hsv_upper_1(255, 255, 255);
+
+        inRange(hsv_frame, hsv_lower_1, hsv_upper_1, violet_mask);
+        //imshow("debug1", yellow_mask);
+
+
+        erode(violet_mask, violet_mask, element_P);
+        erode(violet_mask, violet_mask, element_P);
+        dilate(violet_mask, violet_mask, element_P);
+
+
+
+
+       findContours(violet_mask, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+
+
+       centers.reserve(contours.size());
+       radii.reserve(contours.size());
+       for(int i = 0; i < contours.size(); i++)
+       {
+            minEnclosingCircle(contours[i], centers[i], radii[i]);
+
+
+           circle(frame, Point(int(centers[i].x), int(centers[i].y)), int(radii[i]), radii[i], 2);
+            Scalar color(0, 0, 255);
+           char text [2] = {('0' + i), '\0'};
+           putText(frame, text,Point(int(centers[i].x), int(centers[i].y)),  FONT_HERSHEY_SIMPLEX, 2.5, color);
+
+       }
+
+
+
+        imshow("calibrate", frame);
+         //if esc
+
+        cout << "is input good? [y,n]: " << endl ;
+        c = waitKey(0);
+
+
+        if(c == 'y')
+            break;
+
+
+
+    }
+
+     usleep(5000);
+
+
+    int top_right, top_left, bottom_right, bottom_left;
+    cout << "Index of top right ball: " << endl;
+    c = waitKey(0);
+    usleep(5000);
+
+    top_right = (int)((char)c - '0');
+
+    cout << top_right << endl;
+
+    cout << "Index of top left ball: "<< endl;
+
+    c = waitKey(0);
+     usleep(5000);
+
+     top_left = (int)((char)c - '0');
+
+    cout << top_left << endl;
+
+    cout << "Index of bottom right ball: "<< endl;
+    c = waitKey(0);
+     usleep(5000);
+
+     bottom_right = (int)((char)c - '0');
+
+    cout << bottom_right << endl;
+    cout << "Index of bottom left ball: "<< endl;
+    c = waitKey(0);
+     usleep(5000);
+
+     bottom_left = (int)((char)c - '0');
+
+    cout << bottom_left << endl;
+
+    cout << "OK! " << endl;
+    waitKey(0);
+    destroyWindow("calibrate");
+
+
+    origin = (centers[bottom_left] + centers[bottom_right])/2.0f;
+
+    y_vec = (centers[top_left] + centers[top_right] -(centers[bottom_left] + centers[bottom_right]))/2.0f;
+
+    float normsq = y_vec.dot(y_vec);
+    y_vec = y_vec/normsq;
+
+    return;
+
+
+}
+
+float convert_to_height(cv::Point2f& point)
+{
+    return y_vec.dot(point-origin);
+}
+
+
 float calc_position(Mat& frame)
 {
 
+
+
     Mat hsv_frame, yellow_mask;
+    boxFilter(frame, frame, frame.type(), Size(3,3));
     cvtColor(frame, hsv_frame, CV_BGR2HSV);
 
-    Scalar hsv_lower_1(20, 100, 100);
-    Scalar hsv_upper_1(35, 255, 255);
+    Scalar hsv_lower_1(19, 140, 80);
+    Scalar hsv_upper_1(25, 255, 255);
 
     inRange(hsv_frame, hsv_lower_1, hsv_upper_1, yellow_mask);
-    imshow("debug1", yellow_mask);
+    //imshow("debug1", yellow_mask);
 
 
+    erode(yellow_mask, yellow_mask, element_P);
+    erode(yellow_mask, yellow_mask, element_P);
+    dilate(yellow_mask, yellow_mask, element_M);
+    dilate(yellow_mask, yellow_mask, element_M);
+    //imshow("debug2", yellow_mask);
 
-    erode(yellow_mask, yellow_mask, element);
-    erode(yellow_mask, yellow_mask, element);
-    dilate(yellow_mask, yellow_mask, element);
-    dilate(yellow_mask, yellow_mask, element);
-    imshow("debug2", yellow_mask);
+    Moments m = moments(yellow_mask, false);
+    Point2f p1(m.m10/m.m00, m.m01/m.m00);
 
-    vector<vector<Point> > contours;
+    Scalar color(0, 0, 255);
+    circle(frame, Point(int(p1.x), int(p1.y)), 20, color, 4);
+    imshow("debug3", frame);
+
+    return convert_to_height(p1);
+
+    /*vector<vector<Point> > contours;
+     *
 
 
     findContours(yellow_mask, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
@@ -113,9 +267,9 @@ float calc_position(Mat& frame)
         minEnclosingCircle(contours[min_idx], center, radius);
 
         Scalar color(0, 0, 255);
-        circle(frame, Point(int(center.x), int(center.y)), int(radius), color);
+        circle(frame, Point(int(center.x), int(center.y)), int(radius), color, 4);
         imshow("debug3", frame);
-    }
+    }*/
 
             /*
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
